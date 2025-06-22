8f59e714f2794d9efc28b9f928c22d424fca9141
mittalankit-hub <mittalankit.21.2008@gmail.com>
2025-06-22 09:43:36 +0530

    07:Dashboard Navbar

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Drawer, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { authClient } from "@/lib/auth-client";
 import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
@@ -10,6 +13,7 @@ import { useRouter } from "next/navigation";
 export const DashabordUserButton = () => {
   const {data} = authClient.useSession();
   const router = useRouter();
+  const isMobile =useIsMobile()
 
   const onLogout = () => {
     authClient.signOut({
@@ -28,9 +32,46 @@ export const DashabordUserButton = () => {
     return null;
   }
 
+  if(isMobile) {
+    return(
+      <Drawer>
+        <DrawerTrigger>
+          <div className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
+            {data.user?.image ? (
+              <Avatar>
+                <AvatarImage src={data.user.image} />
+              </Avatar>
+            ) : <GeneratedAvatar seed={data.user.name} variant="initials" className="size-9 mr-3"/> }
+            <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+              <p className="text-sm truncate w-full">{data.user.name}</p>
+              <p className="text-sm truncate w-full">{data.user.email}</p>
+            </div>
+          </div>
+        </DrawerTrigger>
+        <ChevronDownIcon className="shrink-0 size-4" />
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{data.user.name}</DrawerTitle>
+            <DrawerDescription>{data.user.email}</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button variant="outline" onClick={()=>{}}>
+              <CreditCardIcon className="size-4 text-black" />
+              Billing
+            </Button>
+            <Button variant="outline" onClick={onLogout}>
+              <LogOutIcon className="size-4 text-black" />
+              Logout
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    )
+  }
+
   return (
     <DropdownMenu>
-        <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+        <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
             {data.user?.image ? (
                 <Avatar>
                     <AvatarImage src={data.user.image} />

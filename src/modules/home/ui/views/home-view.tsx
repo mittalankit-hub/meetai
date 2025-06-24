258f3b566dc9c948b0f1bfe28dd3e45637594474
mittalankit-hub <mittalankit.21.2008@gmail.com>
2025-06-24 12:57:00 +0000

    08-TRPC Setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,34 +1,17 @@
 "use client"
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { redirect } from "next/navigation";
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 
 
 export const HomePage = ()=>{
-  const {data:session} = authClient.useSession()
-  //console.log("inside home page: Session ->", session)
-
-  if(!session) {
-    return(
-      <p>Loading...</p>
-    )
-  }
+  const trpc = useTRPC()
+  const {data} = useQuery(trpc.hello.queryOptions({text: "Ankit"}))
 
     return (
-      <div className="flex flex-col p-4 gap-4">
-        <p>Logged in as {session?.user?.name} </p>
-        <Button onClick={ ()=> authClient.signOut(
-          {
-            fetchOptions:{
-              onSuccess: () => {
-                redirect("/sign-in");
-              }
-            }
-          }
-        )}>
-          Sign out
-        </Button>
+      <div className="flex flex-col p-4 gap-y-4">
+        {data?.greeting}
       </div>
     )
 }
